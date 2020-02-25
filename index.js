66da952ca0393b928812ddfde94c7d0edb548b56
Mikhail <m_poluektov@ukr.net>
2020-02-25 16:23:41 +0200

    Changen listening port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = 2048
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT + '!');
 });
