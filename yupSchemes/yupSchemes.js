28756d788f70fa1d643a82a6dfd63e065e638f03
Mish-hash <poluektov23@gmail.com>
2020-03-01 19:49:46 +0200

    added requred()

--- yupSchemes/yupSchemes.js
+++ yupSchemes/yupSchemes.js
@@ -3,9 +3,9 @@ const yup = require('yup');
 const idBookScheme = yup.number().positive().integer();
 
 const bookBodyScheme = yup.object().shape({
-    title: yup.string().trim(),
-    author: yup.string().trim(),
-    description: yup.string().trim()
+    title: yup.string().trim().required(),
+    author: yup.string().trim().required(),
+    description: yup.string().trim().required()
 });
 
 
